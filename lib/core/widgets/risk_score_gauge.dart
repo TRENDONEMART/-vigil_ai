bfd86cee2c1a4cbf1788cf0bf087a2585e4dc0ae
Brajesh <trendonemart@gmail.com>
2026-07-21 10:22:29 +0530

    Complete PremiumResultScreen migration for all scanners

--- lib/core/widgets/risk_score_gauge.dart
+++ lib/core/widgets/risk_score_gauge.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class RiskScoreGauge extends StatelessWidget {
+class RiskScoreGauge extends StatefulWidget {
   final int score;
 
   const RiskScoreGauge({
@@ -8,62 +8,139 @@ class RiskScoreGauge extends StatelessWidget {
     required this.score,
   });
 
+  @override
+  State<RiskScoreGauge> createState() => _RiskScoreGaugeState();
+}
+
+class _RiskScoreGaugeState extends State<RiskScoreGauge>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _progressAnimation;
+  late final Animation<int> _scoreAnimation;
+
   Color get _color {
-    if (score >= 80) return Colors.red;
-    if (score >= 60) return Colors.deepOrange;
-    if (score >= 35) return Colors.orange;
+    if (widget.score >= 80) return Colors.red;
+    if (widget.score >= 60) return Colors.deepOrange;
+    if (widget.score >= 35) return Colors.orange;
     return Colors.green;
   }
 
   @override
-  Widget build(BuildContext context) {
-    final progress = score / 100;
+  void initState() {
+    super.initState();
 
-    return SizedBox(
-      width: 170,
-      height: 170,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          SizedBox(
-            width: 170,
-            height: 170,
-            child: TweenAnimationBuilder<double>(
-              tween: Tween(begin: 0, end: progress),
-              duration: const Duration(milliseconds: 1200),
-              curve: Curves.easeOutCubic,
-              builder: (context, value, child) {
-                return CircularProgressIndicator(
-                  value: value,
-                  strokeWidth: 12,
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1400),
+    );
+
+    _progressAnimation = Tween<double>(
+      begin: 0,
+      end: widget.score / 100,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutCubic,
+      ),
+    );
+
+    _scoreAnimation = IntTween(
+      begin: 0,
+      end: widget.score,
+    ).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeOutCubic,
+      ),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void didUpdateWidget(covariant RiskScoreGauge oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (oldWidget.score != widget.score) {
+      _progressAnimation = Tween<double>(
+        begin: 0,
+        end: widget.score / 100,
+      ).animate(
+        CurvedAnimation(
+          parent: _controller,
+          curve: Curves.easeOutCubic,
+        ),
+      );
+
+      _scoreAnimation = IntTween(
+        begin: 0,
+        end: widget.score,
+      ).animate(
+        CurvedAnimation(
+          parent: _controller,
+          curve: Curves.easeOutCubic,
+        ),
+      );
+
+      _controller
+        ..reset()
+        ..forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return SizedBox(
+          width: 180,
+          height: 180,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              SizedBox(
+                width: 180,
+                height: 180,
+                child: CircularProgressIndicator(
+                  value: _progressAnimation.value,
+                  strokeWidth: 13,
+                  strokeCap: StrokeCap.round,
                   backgroundColor: Colors.grey.shade300,
                   valueColor: AlwaysStoppedAnimation(_color),
-                );
-              },
-            ),
-          ),
-          Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(
-                "$score",
-                style: const TextStyle(
-                  fontSize: 42,
-                  fontWeight: FontWeight.bold,
                 ),
               ),
-              const SizedBox(height: 4),
-              const Text(
-                "Risk Score",
-                style: TextStyle(
-                  fontSize: 14,
-                  color: Colors.grey,
-                ),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "${_scoreAnimation.value}",
+                    style: const TextStyle(
+                      fontSize: 46,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  const Text(
+                    "Risk Score",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
